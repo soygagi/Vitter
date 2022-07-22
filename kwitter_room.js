8e5245d36e8fb206517e07856124c8f4c757dc04
soygagi <101847092+soygagi@users.noreply.github.com>
2022-07-21 19:38:46 -0500

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,6 +1,15 @@
-
-//AÑADE TUS ENLACES DE FIREBASE
-
+var firebaseConfig = {
+      apiKey: "AIzaSyBPjrt_PmLsLI24atqxZE6mVCkA4Hz2A64",
+  authDomain: "vitter-3670e.firebaseapp.com",
+  databaseURL: "https://vitter-3670e-default-rtdb.firebaseio.com",
+  projectId: "vitter-3670e",
+  storageBucket: "vitter-3670e.appspot.com",
+  messagingSenderId: "464339231305",
+  appId: "1:464339231305:web:6fb4819c68be53b1695598"
+    };
+firebase.initializeApp(firebaseConfig);
+user_name = localStorage.getItem("user_name");
+document.getElementById("user_name").innerHTML = "¡Bienvenido "+ user_name +"!";
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Inicio del código
