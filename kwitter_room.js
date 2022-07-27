9dddecd49323a9e79acce07021f591c0b238e58b
soygagi <101847092+soygagi@users.noreply.github.com>
2022-07-26 19:40:29 -0500

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -13,7 +13,20 @@ document.getElementById("user_name").innerHTML = "¡Bienvenido "+ user_name +"!"
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Inicio del código
-
+      row = "<div class='room_name' id="+Room_names+" onclick='redirect(this.id)'>#"+Room_names+"</div><hr>";
+      document.getElementById("output").innerHTML += row;
       //Final del código
       });});}
 getData();
+function addRoom(){
+  room_name = document.getElementById("room_name").value;
+  firebase.database().ref("/").child(room_name).update({
+        purpose:"adding room name"
+  });
+  localStorage.setItem("room_name",room_name);
+  window.location = "kwitter_room.html";
+}
+function redirect(name){
+  localStorage.setItem("room_name",name);
+  window.location="index.html";
+}
