f43b00728362bc2b14311159095a899572b2e54c
soygagi <101847092+soygagi@users.noreply.github.com>
2022-08-03 19:38:37 -0500

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -15,7 +15,17 @@ function getData() { firebase.database().ref("/"+room_name).on('value', function
          firebase_message_id = childKey;
          message_data = childData;
 //Inica código
-
+      console.log(firebase_message_id);
+      console.log(message_data)
+      name = message_data['name'];
+      message = message = message_data['message'];
+      like = message_data['like'];
+      name_with_tag = "<h4>"+name+"<img class='user_tick' src='tick.png'></h4>";
+      message_with_data = "<h4 class='message_h4'" + message + "</h4>";
+      like_button = "<button class='btn btn-warning' id="+firebase_message_id+" value="+like+ " onclick='updatelike(this.id)'>";
+      span_width_tag = "<span class='glyphicon glyphicon-thumbs-up'>like: "+like+"</span></button><hr>";
+      row = name_with_tag + message_with_data + like_button + span_with_tag;
+      document.getElementById("output").innerHTML += row;
 //Termina código
       } });  }); }
 getData();
@@ -28,3 +38,13 @@ function send(){
       });
       document.getElementById("msg").value = "";
 }
+function updateLike(message_id)
+ {
+       console.log("botón Me gusta pulsado - " + message_id);
+        button_id = message_id;
+         likes = document.getElementById(button_id).value;
+         updated_likes = Number(likes) + 1; 
+         console.log(updated_likes); 
+         firebase.database().ref(room_name).child(message_id).update({ 
+            like : updated_likes
+       }); }
